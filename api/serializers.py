ef2adef8e3e157b5e54937f7836280019d5f7728
Petr Zakharov <zakharovp11@gmail.com>
2021-08-29 15:57:19 +0300

    pep8

--- api/serializers.py
+++ api/serializers.py
@@ -1,19 +1,31 @@
 from rest_framework import serializers
 
-from .models import Category, Comment, Genre, Review, Title, User
+from .models import (
+    Category,
+    Comment,
+    Genre,
+    Review,
+    Title,
+    User,
+)
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         fields = (
-            'first_name', 'last_name', 'username', 'bio', 'email', 'role'
+            "first_name",
+            "last_name",
+            "username",
+            "bio",
+            "email",
+            "role",
         )
         model = User
         extra_kwargs = {
-            'password': {'required': False},
-            'email': {'required': True}
+            "password": {"required": False},
+            "email": {"required": True},
         }
-        lookup_field = 'username'
+        lookup_field = "username"
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -33,7 +45,9 @@ class GenreSerializer(serializers.ModelSerializer):
 class TitleReadSerializer(serializers.ModelSerializer):
     genre = GenreSerializer(read_only=True, many=True)
     category = CategorySerializer(read_only=True)
-    rating = serializers.IntegerField(read_only=True, required=False)
+    rating = serializers.IntegerField(
+        read_only=True, required=False
+    )
 
     class Meta:
         fields = "__all__"
@@ -42,7 +56,9 @@ class TitleReadSerializer(serializers.ModelSerializer):
 
 class TitleWriteSerializer(TitleReadSerializer):
     genre = serializers.SlugRelatedField(
-        queryset=Genre.objects.all(), slug_field="slug", many=True
+        queryset=Genre.objects.all(),
+        slug_field="slug",
+        many=True,
     )
     category = serializers.SlugRelatedField(
         queryset=Category.objects.all(), slug_field="slug"
@@ -51,35 +67,38 @@ class TitleWriteSerializer(TitleReadSerializer):
 
 class ReviewSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        slug_field='username', read_only=True
+        slug_field="username", read_only=True
     )
 
     def validate(self, data):
-        title = self.context.get('title')
-        request = self.context.get('request')
+        title = self.context.get("title")
+        request = self.context.get("request")
         if (
-            request.method != 'PATCH' and
-            Review.objects.filter(title=title, author=request.user).exists()
+            request.method != "PATCH"
+            and Review.objects.filter(
+                title=title, author=request.user
+            ).exists()
         ):
-            raise serializers.ValidationError('Score already exists')
+            raise serializers.ValidationError(
+                "Score already exists"
+            )
         return data
 
     class Meta:
         model = Review
-        fields = '__all__'
-        extra_kwargs = {'title': {'required': False}}
+        fields = "__all__"
+        extra_kwargs = {"title": {"required": False}}
 
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        read_only=True,
-        slug_field='username'
+        read_only=True, slug_field="username"
     )
 
     class Meta:
-        fields = '__all__'
+        fields = "__all__"
         model = Comment
         extra_kwargs = {
-            'title': {'required': False},
-            'review': {'required': False}
+            "title": {"required": False},
+            "review": {"required": False},
         }
--- api/urls.py
+++ api/urls.py
@@ -4,19 +4,28 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router_v1 = DefaultRouter()
-router_v1.register("users", views.UserViewSet, basename='user')
-router_v1.register('titles', views.TitleViewSet, basename='title')
-router_v1.register('categories', views.CategoryViewSet, basename='category')
-router_v1.register('genres', views.GenreViewSet, basename='genre')
+
 router_v1.register(
-    r'titles/(?P<title_id>[\d]+)/reviews',
-    views.ReviewViewSet,
-    basename='review'
+    "users", views.UserViewSet, basename="user"
 )
 router_v1.register(
-    r'titles/(?P<title_id>[\d]+)/reviews/(?P<review_id>[\d]+)/comments',
+    "titles", views.TitleViewSet, basename="title"
+)
+router_v1.register(
+    "categories", views.CategoryViewSet, basename="category"
+)
+router_v1.register(
+    "genres", views.GenreViewSet, basename="genre"
+)
+router_v1.register(
+    r"titles/(?P<title_id>[\d]+)/reviews",
+    views.ReviewViewSet,
+    basename="review",
+)
+router_v1.register(
+    r"titles/(?P<title_id>[\d]+)/reviews/(?P<review_id>[\d]+)/comments",
     views.CommentViewSet,
-    basename='comment'
+    basename="comment",
 )
 
 urlpatterns = [
